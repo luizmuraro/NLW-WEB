d63e6fde55f472403d82f5b2c5c05d65d6de3e4e
Luiz Guilherme Muraro <luizmuraro@gmail.com>
2020-06-19 02:46:10 -0300

    Create submit function

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState, ChangeEvent } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState, ChangeEvent, FormEvent } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import { Map, TileLayer, Marker } from 'react-leaflet';
 import  { LeafletMouseEvent } from 'leaflet'
@@ -32,7 +32,7 @@ const CreatePoint = () => {
 
     const [formData, setFormData] = useState({
         name: '',
-        emails: '',
+        email: '',
         whatsapp: '',
     })
 
@@ -41,10 +41,11 @@ const CreatePoint = () => {
     const [selectedItems, setSelectedItems] = useState<number[]>([]);
     const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
 
+    const history = useHistory();
+
     useEffect(() => {
         navigator.geolocation.getCurrentPosition(position => {
             const { latitude, longitude } = position.coords;
-
             setInitialPosition([latitude, longitude]);
             console.log(position)
         })
@@ -107,8 +108,27 @@ const CreatePoint = () => {
         }
 
     }
-    function handleSubmit() {
-        
+    async function handleSubmit(event: FormEvent) {
+        event.preventDefault();
+
+        const { name, email, whatsapp } = formData;
+        const uf = selectedUf;
+        const city = selectedCity;
+        const [latitude, longitude] = selectedPosition;
+        const items = selectedItems;
+        const data = {
+            name,
+            email,
+            whatsapp,
+            latitude,
+            longitude,
+            city,
+            uf,
+            items
+        };
+        await api.post('points', data);
+        alert('Ponto de coleta criado!')
+        history.push('/')
     }
     return (
         <div id="page-create-point">
@@ -121,7 +141,7 @@ const CreatePoint = () => {
                 </Link>
             </header>
 
-            <form>
+            <form onSubmit={handleSubmit}>
                 <h1>Cadastro do <br /> ponto de coleta</h1>
 
                 <fieldset>
